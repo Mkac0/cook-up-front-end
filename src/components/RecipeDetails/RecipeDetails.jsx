94154e82de454489081ddf1f184c1bef4903f7f0
pshegde123 <pradnyahegde@gmail.com>
2025-09-30 17:44:03 -0400

    updated recipedetails component layout

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,9 +1,68 @@
+import { useState, useEffect, useContext } from 'react';
+import { useParams, Link } from 'react-router';
+import * as recipeService from '../../services/RecipeService';
+import { UserContext } from '../../contexts/UserContext';
 
 const RecipeDetails = () => {
+    const { recipeId } = useParams();
+    console.log("recipeid = ", recipeId);
+
+    // Access the user object from the UserContext
+    const { user } = useContext(UserContext);
+    const [recipe, setRecipe] = useState(null);
+
+    useEffect(() => {
+        const fetchRecipe = async () => {
+            const recipeData = await recipeService.show(recipeId);
+            console.log("fetched data = ", recipeData);
+            setRecipe(recipeData);
+        };
+        fetchRecipe();
+    }, [recipeId]);
+
+    //console.log("recipe data = ", recipe);
     return (
-        <div>
-            <h1>Recipe Details</h1>
-        </div>
+        <main>
+            <section>
+                <header>
+                    <h1>{recipe.recipeName}</h1>
+                    <p>Prep Time:{recipe.prepTime}</p>
+                    <p>Cook Time:{recipe.cookTime}</p>
+                    <p>Servings:{recipe.servings}</p>
+                    <p>Meal Type:{recipe.mealType}</p>
+                    <p>Dietary Preferance:{recipe.dietaryPreferance}</p>
+                    <p>Description: {recipe.description}</p>
+                    <div>
+                        <h2>Ingredients:</h2>
+                        <ul>
+                            {recipe.ingredients.map((contents) => (
+                                <li>{contents.item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Instructions:</h2>
+                        <ul>
+                            {recipe.instructions.map((instruction) => (
+                                <li>{instruction}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Chef Notes:</h2>
+                        <p>{recipe.chefNotes}</p>
+                    </div>
+                    <div>
+                        <h2>Comments:</h2>
+                        <ul>
+                            {recipe.comments.map((comment) => (
+                                <li key={comment._id}>{comment.text}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </header>
+            </section>
+        </main>
     )
 }
 
--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -18,7 +18,17 @@ const index = async () => {
     throw new Error(err);
   }
 }
-
+const show = async (recipeId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${recipeId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 export {
     index,
+    show,
 }
